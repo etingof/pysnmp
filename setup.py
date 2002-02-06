b2da640a16b14f651bd0eccc07242160178e2c37
elie <elie>
2002-02-06 10:11:57 +0000

    setup.py has been missed.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="pysnmp",
+      version="2.0.1",
+      description="Python SNMP Toolkit",
+      author="Ilya Etingof",
+      author_email="ilya@glas.net ",
+      url="http://sourceforge.net/projects/pysnmp/",
+      packages=['pysnmp'],
+      )
