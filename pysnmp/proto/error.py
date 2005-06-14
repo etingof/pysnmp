ae935c1fe3d0f7140701104c480dff3f2d9a8815
elie <elie>
2005-06-14 10:02:45 +0000

    Lame exceptions reduced, StatusInformation added

--- pysnmp/proto/error.py
+++ pysnmp/proto/error.py
@@ -1,11 +1,20 @@
-from pysnmp import error
+from pyasn1.error import PyAsn1Error
+from pysnmp.error import PySnmpError
 
-class ProtoError(error.PySnmpError): pass
-class BadArgumentError(ProtoError): pass
+class ProtocolError(PySnmpError, PyAsn1Error): pass
 
 # SNMP v3 exceptions
 
-class SnmpV3Error(ProtoError): pass
+class SnmpV3Error(ProtocolError): pass
+class StatusInformation(SnmpV3Error):
+    def __init__(self, **kwargs):
+#        print kwargs
+        self.__errorIndication = kwargs
+    def __str__(self): return str(self.__errorIndication)
+    def __getitem__(self, key): return self.__errorIndication[key]
+    def has_key(self, key): return self.__errorIndication.has_key(key)
+    def get(self, key, defVal=None):
+        return self.__errorIndication.get(key, defVal)
 class CacheExpiredError(SnmpV3Error): pass
 class InternalError(SnmpV3Error): pass
 class MessageProcessingError(SnmpV3Error): pass
