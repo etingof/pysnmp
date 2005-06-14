148647aba8fabb52ff3f7a6c53e716614146cfb6
elie <elie>
2005-06-14 10:10:53 +0000

    * abstract API changed from named to positional parameters * cache management code added

--- pysnmp/proto/secmod/base.py
+++ pysnmp/proto/secmod/base.py
@@ -1,21 +1,76 @@
-from pysnmp.proto.secmod import error
+from pysnmp.proto import error
 
 class AbstractSecurityModel:
-    def __init__(self, mibInstrumController=None):
-        self.mibInstrumController = mibInstrumController
+    securityModelID = None
+    __stateReference = 0L
+    def __init__(self):
         self.__cacheEntries = {}
+
+    def processIncomingMsg(
+        self,
+        mibInstrumController,
+        messageProcessingModel,
+        maxMessageSize,
+        securityParameters,
+        securityModel,
+        securityLevel,
+        wholeMsg,
+        msg,
+        ):
+        raise error.ProtocolError(
+            'Security model %s not implemented' % self
+            )
+
+    def generateRequestMsg(
+        self,
+        minInstrController,
+        messageProcessingModel,
+        globalData,
+        maxMessageSize,
+        securityModel,
+        securityEngineID,
+        securityName,
+        securityLevel,
+        scopedPDU,
+        ):
+        raise error.ProtocolError(
+            'Security model %s not implemented' % self
+            )
+
+    def generateResponseMsg(
+        self,
+        minInstrController,
+        messageProcessingModel,
+        globalData,
+        maxMessageSize,
+        securityModel,
+        securityEngineID,
+        securityName,
+        securityLevel,
+        scopedPDU,
+        securityStateReference
+        ):
+        raise error.ProtocolError(
+            'Security model %s not implemented' % self
+            )
+
+    # Caching stuff
     
-    def processIncomingMsg(self, msg, **kwargs):
-        raise error.BadArgumentError(
-            'Security model %s not implemented' % self
-            )
+    def _cachePush(self, **securityData):
+        stateReference = AbstractSecurityModel.__stateReference
+        AbstractSecurityModel.__stateReference = stateReference + 1
+        self.__cacheEntries[stateReference] = securityData
+        return stateReference
+    
+    def _cachePop(self, stateReference):
+        securityData = self.__cacheEntries.get(stateReference)
+        if securityData is None:
+            raise error.ProtocolError(
+                'Cache miss for stateReference=%s at %s' %
+                (stateReference, self)
+                )
+        del self.__cacheEntries[stateReference]
+        return securityData
 
-    def generateRequestMsg(self, msg, **kwargs):
-        raise error.BadArgumentError(
-            'Security model %s not implemented' % self
-            )
-
-    def generateResponseMsg(self, msg, **kwargs):
-        raise error.BadArgumentError(
-            'Security model %s not implemented' % self
-            )
+    def releaseStateInformation(self, stateReference):
+        self.__cachePop(stateReference)
