3c31ca81c65edec3889f71105cf5979beafd0522
elie <elie>
2005-06-14 10:19:29 +0000

    Inherit ASN1 module classes from SMIv2 classes rather than from original ASN1 ones for SNMPv2 SMI compatibility. This may need to be reworked.

--- pysnmp/smi/mibs/ASN1.py
+++ pysnmp/smi/mibs/ASN1.py
@@ -1,9 +1,15 @@
-# Base ASN.1 objects
+# ASN.1 objects used in SNMP
+from pyasn1.type import univ
+from pysnmp.proto import rfc1902
 
-from pysnmp.asn1 import univ
+Integer = rfc1902.Integer32
+OctetString = rfc1902.OctetString
 
-Integer = univ.Integer
-OctetString = univ.OctetString
+# Instead of using base ASN,1 types we use SNMPv2 SMI ones to make
+# SMI objects type-compatible with SNMP protocol values
+
+# Integer = univ.Integer
+# OctetString = univ.OctetString
 BitString = univ.BitString
 Null = univ.Null
 ObjectIdentifier = univ.ObjectIdentifier
