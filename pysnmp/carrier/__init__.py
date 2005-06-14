ae5e5211c25ee13d45887ee8c1fa18d885cfdc8f
elie <elie>
2005-06-14 09:50:38 +0000

    docstring removed

--- pysnmp/carrier/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-   SNMP framework for Python.
-
-   The pysnmp.mapping sub-package implements various transport mappings
-   for SNMP.
-
-   Copyright 1999-2004 by Ilya Etingof <ilya@glas.net>. See LICENSE for details.
-"""
