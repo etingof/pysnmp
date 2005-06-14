782d0941ae26a267756c1a740a3dcefd464dd69b
elie <elie>
2005-06-14 10:11:59 +0000

    converted to positional params API the v1/v2c model is currently broken

--- pysnmp/proto/secmod/rfc2576.py
+++ pysnmp/proto/secmod/rfc2576.py
@@ -1,38 +1,36 @@
 # SNMP v1 & v2c security models implementation
-from pysnmp.proto.secmod import base, error
+from pysnmp.proto.secmod import base
 from pysnmp.proto import rfc1157, rfc1905, rfc3411
 from pysnmp.smi.error import NoSuchInstanceError
-
-__all__ = [ 'SnmpV1SecurityModel', 'SnmpV2cSecurityModel' ]
+from pysnmp.proto import error
 
 class SnmpV1SecurityModel(base.AbstractSecurityModel):
-    # Map PDU to PDU key names at PDUS choice
-    _pduMap = {
-        # SNMP v1
-        rfc1157.GetRequestPdu.tagSet: 'get_request',
-        rfc1157.GetNextRequestPdu.tagSet: 'get_next_request',
-        rfc1157.GetResponsePdu.tagSet: 'get_response',
-        rfc1157.SetRequestPdu.tagSet: 'set_request',
-        rfc1157.TrapPdu.tagSet: 'trap',
-        }
-
-    _protoMsg = rfc1157.Message
-    def __init__(self, mibInstrumController=None):
-        base.AbstractSecurityModel.__init__(self, mibInstrumController)
-        self.__msg = self._protoMsg()
-        
+    securityModelID = 1
     # According to rfc2576, community name <-> contextEngineID/contextName
     # mapping is up to MP module for notifications but belongs to secmod
     # responsibility for other PDU types. Since I do not yet understand
-    # the reason for such distribution, I moved this code from MP-scope
+    # the reason for this distribution, I moved this code from MP-scope
     # in here.
 
-    def generateRequestMsg(self, **kwargs):
+    def generateRequestMsg(
+        self,
+        snmpEngine,
+        messageProcessingModel,
+        globalData,
+        maxMessageSize,
+        securityModel,
+        securityEngineID,
+        securityName,
+        securityLevel,
+        scopedPDU
+        ):
+        contextEngineID, contextName, pdu = scopedPDU
+        
         # rfc2576: 5.2.3
-        snmpCommunityName, \
-        snmpCommunitySecurityName, \
-        snmpCommunityContextEngineID, \
-        snmpCommunityContextName = self.mibInstrumController.mibBuilder.importSymbols(
+        ( snmpCommunityName,
+          snmpCommunitySecurityName, 
+          snmpCommunityContextEngineID, 
+          snmpCommunityContextName ) = snmpEngine.msgAndPduDsp.mibInstrumController.mibBuilder.importSymbols(
             'SNMP-COMMUNITY-MIB',
             'snmpCommunityName',
             'snmpCommunitySecurityName',
@@ -47,63 +45,71 @@ class SnmpV1SecurityModel(base.AbstractSecurityModel):
                     )
             except NoSuchInstanceError:
                 break
-            if mibNodeIdx.syntax != kwargs.get('securityName'):
+            if mibNodeIdx.syntax != securityName:
                 continue
             instId = mibNodeIdx.name[len(snmpCommunitySecurityName.name):]
             mibNode = snmpCommunityContextEngineID.getNode(
                 snmpCommunityContextEngineID.name + instId
                 )
-            if mibNode.syntax != kwargs.get('contextEngineID'):
+            if mibNode.syntax != contextEngineID:
                 continue
             mibNode = snmpCommunityContextName.getNode(
                 snmpCommunityContextName.name + instId
                 )
-            if mibNode.syntax != kwargs.get('contextName'):
+            if mibNode.syntax != contextName:
                 continue
+            # XXX TODO: snmpCommunityTransportTag
             mibNode = snmpCommunityName.getNode(
                 snmpCommunityName.name + instId
                 )
-            communityName = mibNode.syntax.get()
-            self.__msg['community'].set(communityName)
-            transportDomain, transportAddress, pdu = kwargs['scopedPDU']
-            self.__msg['pdu'][self._pduMap[pdu.tagSet]] = pdu
-            return {
-                'securityParameters': communityName,
-                'wholeMsg': self.__msg.berEncode()
-                }
-        raise error.BadArgumentError(
-            'Can\'t resolve community name to contextEngineID/Name'
+            communityName = mibNode.syntax
+            securityParameters = communityName
+            return ( securityParameters, scopedPDU )
+
+        raise error.StatusInformation(
+            errorIndication = 'unknownCommunityName'
             )
 
-    def generateResponseMsg(self, **kwargs):
+    def generateResponseMsg(
+        self,
+        snmpEngine,
+        messageProcessingModel,
+        globalData,
+        maxMessageSize,
+        securityModel,
+        securityEngineID,
+        securityName,
+        securityLevel,
+        scopedPDU,
+        securityStateReference
+        ):
         # rfc2576: 5.2.2
-        securityParameters = kwargs['securityStateReference']        
-        communityName = securityParameters
-        self.__msg['community'].set(communityName)
-        transportDomain, transportAddress, pdu = kwargs['scopedPDU']
-        self.__msg['pdu'][self._pduMap[pdu.tagSet]] = pdu
-        return {
-            'securityParameters': securityParameters,
-            'wholeMsg': self.__msg.berEncode()
-            }
-
-    def processIncomingMsg(self, **kwargs):
-        self.__msg.decodeItem(kwargs['wholeMsg'])
-        
-        # rfc2576: 5.2.1
-        transportDomain, transportAddress = kwargs.get(
-            'securityParameters'
+        cachedData = self._cachePop(securityStateReference)
+        securityParameters = (
+            cachedData['communityName'],
+            cachedData['srcTransport'],
+            cachedData['destTransport']
             )
-        
-        # According to rfc2576 this should be done at MP but I don't yet see
-        # the reason why it wouldn't be better done at SM
-        communityName = self.__msg['community']
+        return ( securityParameters, scopedPDU )
 
+    def processIncomingMsg(
+        self,
+        snmpEngine,
+        messageProcessingModel,
+        maxMessageSize,
+        securityParameters,
+        securityModel,
+        securityLevel,
+        wholeMsg,
+        msg
+        ):
+        # rfc2576: 5.2.1
+        ( communityName, srcTransport, destTransport ) = securityParameters
         ( snmpCommunityName,
           snmpCommunitySecurityName,
           snmpCommunityContextEngineID,
-          snmpCommunityContextName ) = \
-          self.mibInstrumController.mibBuilder.importSymbols(
+          snmpCommunityContextName
+          ) = snmpEngine.msgAndPduDsp.mibInstrumController.mibBuilder.importSymbols(
             'SNMP-COMMUNITY-MIB',
             'snmpCommunityName',
             'snmpCommunitySecurityName',
@@ -122,9 +128,13 @@ class SnmpV1SecurityModel(base.AbstractSecurityModel):
                 continue
             break
         else:
-            raise error.BadArgumentError(
-                'No matching community name found'
+            snmpInBadCommunityNames, = snmpEngine.msgAndPduDsp.mibInstrumController.mibBuilder.importSymbols('SNMP-COMMUNITY-MIB', 'snmpInBadCommunityNames')
+            snmpInBadCommunityNames.syntax = snmpInBadCommunityNames.syntax+1
+            raise error.StatusInformation(
+                errorIndication = 'unknownCommunityName'
                 )
+
+        # XXX TODO: snmpCommunityTransportTag 
         instId = mibNodeIdx.name[len(snmpCommunityName.name):]
         communityName = snmpCommunityName.getNode(
             snmpCommunityName.name + instId
@@ -138,60 +148,38 @@ class SnmpV1SecurityModel(base.AbstractSecurityModel):
         contextName = snmpCommunityContextName.getNode(
             snmpCommunityContextName.name + instId
             )
-        snmpEngineID, = self.mibInstrumController.mibBuilder.importSymbols(
+        snmpEngineID, = snmpEngine.msgAndPduDsp.mibInstrumController.mibBuilder.importSymbols(
             'SNMP-FRAMEWORK-MIB', 'snmpEngineID'
             )
-        return {
-            'securityEngineID': snmpEngineID.syntax.get(),
-            'securityName': securityName.syntax.get(),
-            'scopedPDU': (
-            contextEngineID.syntax.get(), contextName.syntax.get(),
-            self.__msg['pdu'].values()[0]
-            ),
-            'maxSizeResponseScopedPDU': 65000, # XXX
-            'securityStateReference': communityName.syntax.get()
-            }
+
+        stateReference = self._cachePush(
+            communityName=communityName.syntax,
+            srcTransport=srcTransport,
+            destTransport=destTransport
+            )
+        
+        securityEngineID = snmpEngineID.syntax
+        securityName = securityName.syntax
+        scopedPDU = (
+            contextEngineID.syntax, contextName.syntax,
+            msg.getComponentByPosition(2).getComponent()
+            )
+        maxSizeResponseScopedPDU = maxMessageSize - 128
+        securityStateReference = stateReference
+        
+        return ( securityEngineID,
+                 securityName,
+                 scopedPDU,
+                 maxSizeResponseScopedPDU,
+                 securityStateReference )
     
 class SnmpV2cSecurityModel(SnmpV1SecurityModel):
-    _pduMap = {
-        # SNMP v2c
-        rfc1905.GetRequestPdu.tagSet: 'get_request',
-        rfc1905.GetNextRequestPdu.tagSet: 'get_next_request',
-        rfc1905.GetBulkRequestPdu.tagSet: 'get_bulk_request',
-        rfc1905.ResponsePdu.tagSet: 'response',
-        rfc1905.SetRequestPdu.tagSet: 'set_request',
-        rfc1905.InformRequestPdu.tagSet: 'inform_request',
-        rfc1905.SnmpV2TrapPdu.tagSet: 'snmpV2_trap',
-        rfc1905.ReportPdu.tagSet: 'report'
-        }
-    _protoMsg = rfc1905.Message
+    securityModelID = 2
+    _protoMsg = rfc1157.Message() # XXX re-assign PDUs
     
-if __name__ == '__main__':
-    from pysnmp.proto import rfc1157
-    from pysnmp.smi.objects import module
-
-    mib = module.MibModules().loadModules()
-
-    row = mib.getVariable((1,3,6,1,6,3,18,1,1,1))
-    mib.writeVars(
-        (row.getInstNameByIndex(2, 'myrouter'), 'mycomm'),
-        (row.getInstNameByIndex(3, 'myrouter'), 'myrt'),
-    )
-
-    sm = SnmpV1SecurityModel(mib)
-    smInParams = {
-        'scopedPDU': ('', '', rfc1157.GetRequestPdu()),
-        'securityName': 'myrt',
-        'contextEngineID': '80004fb81c0a80101',
-        'contextName': ''
-        }
-
-    smOutParams = apply(sm.generateRequestMsg, (), smInParams)
-    print smOutParams
-
-    smInParams = {
-        'securityParameters': ('mycomm', '', ''),
-        'wholeMsg': smOutParams['wholeMsg']
-        }
-
-    print apply(sm.processIncomingMsg, (), smInParams)    
+# XXX
+# use biling api?
+# move to pyunit
+# v2c support
+# where to set defaults to PDU?
+# contextEngineId/contextName goes to globalData
