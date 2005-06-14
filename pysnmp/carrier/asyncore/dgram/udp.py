600504b5648a657483e1315de66d5c05f2fd4cfc
elie <elie>
2005-06-14 10:00:57 +0000

    transport domain & domain-specific address re-writing changed

--- pysnmp/carrier/asyncore/dgram/udp.py
+++ pysnmp/carrier/asyncore/dgram/udp.py
@@ -1,25 +1,8 @@
 """Implements asyncore-based UDP transport domain"""
 from socket import AF_INET
-from types import TupleType, IntType
-from string import atoi
 from pysnmp.carrier.asynsock.dgram.base import DgramSocketTransport
-from pysnmp.carrier import error
+
+domainName = snmpUDPDomain = (1, 3, 6, 1, 6, 1, 1)
 
 class UdpSocketTransport(DgramSocketTransport):
     sockFamily = AF_INET
-    defaultPort = 161
-
-    def rewriteAddress(self, transportAddress):
-        if type(transportAddress) == TupleType:
-            if type(transportAddress[1]) != IntType:
-                try:
-                    return transportAddress[0], atoi(transportAddress[1])
-                except ValueError, why:
-                    raise error.BadArgumentError(
-                        'Cant coerce UDP port number %s: %s' %
-                        (transportAddress[1], why)
-                        )
-            else:
-                return transportAddress[0], transportAddress[1]
-        else:
-            return transportAddress, self.defaultPort
