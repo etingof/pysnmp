4ac44dc17214fe1f2fe04b76942001ae9040ba84
elie <elie>
2005-06-14 09:59:42 +0000

    minor layout changes

--- pysnmp/carrier/asyncore/dgram/base.py
+++ pysnmp/carrier/asyncore/dgram/base.py
@@ -1,11 +1,10 @@
 """Implements asyncore-based generic DGRAM transport"""
-from socket import SOCK_DGRAM, error
-from errno import EWOULDBLOCK, ECONNRESET, ENOTCONN, ESHUTDOWN
+import socket, errno
 from pysnmp.carrier.asynsock.base import AbstractSocketTransport
 from pysnmp.carrier import error
 
 class DgramSocketTransport(AbstractSocketTransport):
-    sockType = SOCK_DGRAM
+    sockType = socket.SOCK_DGRAM
     retryCount = 3; retryInterval = 1
     def __init__(self, sock=None, sockMap=None):
         self.__outQueue = []
@@ -15,14 +14,14 @@ class DgramSocketTransport(AbstractSocketTransport):
         if iface is not None:
             try:
                 self.socket.bind(iface)
-            except error, why:
+            except socket.error, why:
                 raise error.CarrierError('bind() failed: %s' % why)
         return self
     
     def openServerMode(self, iface):
         try:
             self.socket.bind(iface)
-        except error, why:
+        except socket.error, why:
             raise error.CarrierError('bind() failed: %s' % why)
         self._iface = iface
         return self
@@ -41,9 +40,9 @@ class DgramSocketTransport(AbstractSocketTransport):
         outgoingMessage, transportAddress = self.__outQueue.pop()
         try:
             self.socket.sendto(outgoingMessage, transportAddress)
-        except error, why:
-            if why[0] != EWOULDBLOCK:
-                raise error, why
+        except socket.error, why:
+            if why[0] != socket.EWOULDBLOCK:
+                raise socket.error, why
     def readable(self): return 1
     def handle_read(self):
         try:
@@ -54,10 +53,10 @@ class DgramSocketTransport(AbstractSocketTransport):
             else:
                 self._cbFun(self, transportAddress, incomingMessage)
                 return
-        except error, why:
+        except socket.error, why:
             # winsock sometimes throws ENOTCONN
-            if why[0] in [ECONNRESET, ENOTCONN, ESHUTDOWN]:
+            if why[0] in (socket.ECONNRESET, socket.ENOTCONN, socket.ESHUTDOWN):
                 self.handle_close()
                 return
             else:
-                raise error, why
+                raise socket.error, why
