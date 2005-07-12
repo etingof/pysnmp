301e485daec060937fc683373b05464eb182ad8b
elie <elie>
2005-07-12 10:47:40 +0000

    initial revision

---
+++ pysnmp/entity/rfc3413/context.py
@@ -0,0 +1,31 @@
+# SNMP entity context
+from pysnmp import error
+
+class SnmpContext:
+    def __init__(self, snmpEngine, contextEngineId=None):
+        self.contextEngineId = contextEngineId
+        self.contextNames = {
+            '': snmpEngine.msgAndPduDsp.mibInstrumController # Default name
+            } 
+
+    def registerContextName(self, contextName, mibInstrum=None):
+        if self.contextNames.has_key(contextName):
+            raise error.PySnmpError(
+                'Duplicate contextName %s' % contextName
+                )
+        if mibInstrum is None:
+            self.contextName[contextName] = self.contextName['']
+        else:
+            self.contextName[contextName] = mibInstrum
+            
+    def unregisterContextName(self, contextName):
+        if self.contextNames.has_key(contextName):
+            del self.contextName[contextName]
+
+    def getMibInstrum(self, contextName):
+        if not self.contextNames.has_key(contextName):
+            raise error.PySnmpError(
+                'Missing contextName %s' % contextName
+                )
+        else:
+            return self.contextNames[contextName]
