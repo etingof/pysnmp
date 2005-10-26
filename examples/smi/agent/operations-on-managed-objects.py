40efc8f4e1967f74d893f271fa97b5aba44b38d0
elie <elie>
2005-10-26 22:29:46 +0000

    moved from instrum.py

---
+++ examples/smi/agent/operations-on-managed-objects.py
@@ -0,0 +1,26 @@
+# SNMP agent backend e.g. Agent access to Managed Objects
+from pysnmp.smi import builder, instrum, exval
+
+print 'Loading MIB modules...',
+mibBuilder = builder.MibBuilder().loadModules(
+    'SNMPv2-MIB', 'SNMP-FRAMEWORK-MIB', 'SNMP-COMMUNITY-MIB'
+    )
+print 'done'
+
+print 'Building MIB tree...',
+mibInstrum = instrum.MibInstrumController(mibBuilder)
+print 'done'
+
+print 'Remote manager write/create access to MIB instrumentation: ',
+print mibInstrum.writeVars(
+    (((1,3,6,1,6,3,18,1,1,1,2,109,121,110,109,115), 'mycomm'),
+     ((1,3,6,1,6,3,18,1,1,1,3,109,121,110,109,115), 'mynmsname'))
+    )
+
+print 'Remote manager read access to MIB instrumentation (table walk)'
+oid, val = (), None
+while 1:
+    oid, val = mibInstrum.readNextVars(((oid, val),))[0]
+    if exval.endOfMib.isSameTypeWith(val):
+        break
+    print oid, val
