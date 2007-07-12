5e139229b7eec0449fcdc55f3f2e54c28821a809
elie <elie>
2007-07-12 16:21:50 +0000

    swap tagIds of noSuchInstance with noSuchObject

--- pysnmp/smi/exval.py
+++ pysnmp/smi/exval.py
@@ -1,6 +1,6 @@
 from pyasn1.type import univ, tag
 
-noSuchInstance = univ.Null('').subtype(implicitTag=tag.Tag(tag.tagClassContext, tag.tagFormatSimple, 0x00))
-noSuchObject = univ.Null('').subtype(implicitTag=tag.Tag(tag.tagClassContext, tag.tagFormatSimple, 0x01))
+noSuchObject = univ.Null('').subtype(implicitTag=tag.Tag(tag.tagClassContext, tag.tagFormatSimple, 0x00))
+noSuchInstance = univ.Null('').subtype(implicitTag=tag.Tag(tag.tagClassContext, tag.tagFormatSimple, 0x01))
 endOfMib = univ.Null('').subtype(implicitTag=tag.Tag(tag.tagClassContext, tag.tagFormatSimple, 0x02))
 
