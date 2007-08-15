de9d15e6a1cca45de1441936db38620c12093baf
elie <elie>
2007-08-15 10:02:37 +0000

    4.1.9a

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name="pysnmp",
-      version="4.1.8a",
+      version="4.1.9a",
       description="SNMP framework for Python",
       author="Ilya Etingof",
       author_email="ilya@glas.net ",
