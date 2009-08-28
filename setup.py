5d07064bbaccdad55df64d505390cfb9eb4990a9
elie <elie>
2009-08-28 19:37:36 +0000

    attempt to load setuptools

--- setup.py
+++ setup.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
-from distutils.core import setup
+import sys
+
+def howto_install_setuptools():
+    print """Error: You need setuptools Python package!
+
+It's very easy to install it, just type (as root on Linux):
+   wget http://peak.telecommunity.com/dist/ez_setup.py
+   python ez_setup.py
+"""
+
+try:
+    from setuptools import setup
+except ImportError:
+    for arg in sys.argv:
+        if "egg" in arg:
+            howto_install_setuptools()
+            sys.exit(1)
+    from distutils.core import setup
 
 setup(name="pysnmp",
       version="4.1.12a",
