9965bcc4a7c9538399f00ee581d9f82de3672805
elie <elie>
2010-06-23 12:22:22 +0000

    add debugging for context operations

--- pysnmp/entity/rfc3413/context.py
+++ pysnmp/entity/rfc3413/context.py
@@ -1,5 +1,6 @@
 # SNMP entity context
 from pysnmp import error
+from pysnmp import debug
 
 class SnmpContext:
     def __init__(self, snmpEngine, contextEngineId=None):
@@ -8,6 +9,7 @@ class SnmpContext:
             contextEngineId,= snmpEngine.msgAndPduDsp.mibInstrumController.mibBuilder.importSymbols('__SNMP-FRAMEWORK-MIB', 'snmpEngineID')
             contextEngineId = contextEngineId.syntax
         self.contextEngineId = contextEngineId
+        debug.logger & debug.flagIns and debug.logger('SnmpContext: contextEngineId \"%s\"' % contextEngineId)
         self.contextNames = {
             '': snmpEngine.msgAndPduDsp.mibInstrumController # Default name
             } 
@@ -17,6 +19,7 @@ class SnmpContext:
             raise error.PySnmpError(
                 'Duplicate contextName %s' % contextName
                 )
+        debug.logger & debug.flagIns and debug.logger('registerContextName: registered contextName \"%s\", mibInstrum %s' % (contextName, mibInstrum))
         if mibInstrum is None:
             self.contextNames[contextName] = self.contextNames['']
         else:
@@ -24,12 +27,15 @@ class SnmpContext:
             
     def unregisterContextName(self, contextName):
         if self.contextNames.has_key(contextName):
+            debug.logger & debug.flagIns and debug.logger('unregisterContextName: unregistered contextName \"%s\"' % contextName)
             del self.contextNames[contextName]
 
     def getMibInstrum(self, contextName):
         if not self.contextNames.has_key(contextName):
+            debug.logger & debug.flagIns and debug.logger('getMibInstrum: contextName \"%s\" not registered' % contextName)
             raise error.PySnmpError(
                 'Missing contextName %s' % contextName
                 )
         else:
+            debug.logger & debug.flagIns and debug.logger('getMibInstrum: contextName \"%s\", mibInstum %s' % (contextName, self.contextNames[contextName]))
             return self.contextNames[contextName]
