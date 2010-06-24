6c6728a9fa75cbf5dad50de60795de1d16d64891
elie <elie>
2010-06-24 18:54:33 +0000

    * PyCrypto AES implementation seems to require plain/ciphertext padding * set AES CFB mode segment size to 128 bit * fixed salt ordering in IV generation * salt value randomness improved

--- pysnmp/proto/secmod/rfc3826/priv/aes.py
+++ pysnmp/proto/secmod/rfc3826/priv/aes.py
@@ -4,6 +4,11 @@ from pyasn1.type import univ
 from pysnmp.proto import error
 
 try:
+    from sys import version_info
+except ImportError:
+    version_info = ( 0, 0 )   # a really early version
+
+try:
     from Crypto.Cipher import AES
 except ImportError:
     AES = None
@@ -16,7 +21,10 @@ random.seed()
 
 class Aes(base.AbstractEncryptionService):
     serviceID = (1, 3, 6, 1, 6, 3, 10, 1, 2, 4) # usmAesCfb128Protocol
-    _localInt = long(random.random()*0xffffffffffffffffL)
+    if version_info < (2, 3):
+        _localInt = long(random.random()*0xffffffffffffffffL)
+    else:
+        _localInt = random.randrange(0, 0xffffffffffffffffL)
     # 3.1.2.1
     def __getEncryptionKey(self, privKey, snmpEngineBoots, snmpEngineTime):
         salt = [
@@ -35,7 +43,7 @@ class Aes(base.AbstractEncryptionService):
         else:
             self._localInt = self._localInt + 1
 
-        salt = string.join(map(lambda x: chr(x), salt), '')
+        salt = string.join(map(chr, salt), '')
 
         return self.__getDecryptionKey(privKey, snmpEngineBoots,
                                        snmpEngineTime, salt) + ( salt, )
@@ -55,17 +63,17 @@ class Aes(base.AbstractEncryptionService):
             snmpEngineTime>>16&0xff,
             snmpEngineTime>>8&0xff,
             snmpEngineTime&0xff,
-            ord(salt[7]),
-            ord(salt[6]),
-            ord(salt[5]),
-            ord(salt[4]),
-            ord(salt[3]),
-            ord(salt[2]),
+            ord(salt[0]),
             ord(salt[1]),
-            ord(salt[0])
+            ord(salt[2]),
+            ord(salt[3]),
+            ord(salt[4]),
+            ord(salt[5]),
+            ord(salt[6]),
+            ord(salt[7])
             ]
 
-        return privKey[:16], string.join(map(lambda x: chr(x), iv), '')
+        return privKey[:16], string.join(map(chr, iv), '')
 
         
     # 3.2.4.1
@@ -74,20 +82,24 @@ class Aes(base.AbstractEncryptionService):
             raise error.StatusInformation(
                 errorIndication='encryptionError'
                 )
-
+ 
         snmpEngineBoots, snmpEngineTime, salt = privParameters
-        
+
         # 3.3.1.1
         aesKey, iv, salt = self.__getEncryptionKey(
             str(encryptKey), snmpEngineBoots, snmpEngineTime
             )
 
         # 3.3.1.3
-        aesObj = AES.new(aesKey, AES.MODE_CFB, iv)
+        aesObj = AES.new(aesKey, AES.MODE_CFB, iv, segment_size=128)
+
+        # PyCrypto seems to require padding
+        dataToEncrypt = dataToEncrypt + '\0' * (16-len(dataToEncrypt)%16)
+
         ciphertext = aesObj.encrypt(dataToEncrypt)
 
         # 3.3.1.4
-        return univ.OctetString(ciphertext), salt
+        return univ.OctetString(ciphertext), univ.OctetString(salt)
         
     # 3.2.4.2
     def decryptData(self, decryptKey, privParameters, encryptedData):
@@ -109,7 +121,10 @@ class Aes(base.AbstractEncryptionService):
             str(decryptKey), snmpEngineBoots, snmpEngineTime, salt
             )
 
-        aesObj = AES.new(aesKey, AES.MODE_CFB, iv)
+        aesObj = AES.new(aesKey, AES.MODE_CFB, iv, segment_size=128)
+
+        # PyCrypto seems to require padding
+        encryptedData = encryptedData + '\0' * (16-len(encryptedData)%16)
 
         # 3.3.2.4-6
         return aesObj.decrypt(str(encryptedData))
