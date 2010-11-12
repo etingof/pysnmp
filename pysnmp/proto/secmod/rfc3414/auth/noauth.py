05b678da1ac18cb09c14a8b1f9cfdd515299b522
elie <elie>
2010-11-12 17:36:52 +0000

    fix to noauth/nopriv security module so that it would not crash SNMP engine if called accidentally (e.g. authenticated message comes for a local noAuth user)

--- pysnmp/proto/secmod/rfc3414/auth/noauth.py
+++ pysnmp/proto/secmod/rfc3414/auth/noauth.py
@@ -1,4 +1,11 @@
 from pysnmp.proto.secmod.rfc3414.auth import base
+from pysnmp.proto import error
 
 class NoAuth(base.AbstractAuthenticationService):
     serviceID = (1, 3, 6, 1, 6, 3, 10, 1, 1, 1)  # usmNoAuthProtocol
+    # 7.2.4.2
+    def authenticateOutgoingMsg(self, authKey, wholeMsg):
+        raise error.StatusInformation(errorIndication='no authentication')
+
+    def authenticateIncomingMsg(self, authKey, authParameters, wholeMsg):
+        raise error.StatusInformation(errorIndication='no authentication')
--- pysnmp/proto/secmod/rfc3414/priv/nopriv.py
+++ pysnmp/proto/secmod/rfc3414/priv/nopriv.py
@@ -1,4 +1,12 @@
 from pysnmp.proto.secmod.rfc3414.priv import base
+from pysnmp.proto import error
 
 class NoPriv(base.AbstractEncryptionService):
     serviceID = (1, 3, 6, 1, 6, 3, 10, 1, 2, 1) # usmNoPrivProtocol
+    def encryptData(self, mibInstrumController, encryptKey,
+                    dataToEncrypt):
+        raise error.StatusInformation(errorIndication='no encryption')
+    
+    def decryptData(self, mibInstrumController, decryptKey,
+                    privParameters, encryptedData):
+        raise error.StatusInformation(errorIndication='no encryption')
