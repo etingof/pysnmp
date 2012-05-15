19e368aed8fb9f4ee0ab350b28058aa27ca656be
elie <elie>
2012-05-15 11:00:48 +0000

    replace pyasn1 exceptions with pysnmp one

--- pysnmp/proto/api/verdec.py
+++ pysnmp/proto/api/verdec.py
@@ -1,6 +1,11 @@
 from pyasn1.codec.ber import decoder
+from pyasn1.error import PyAsn1Error
+from pysnmp.proto.error import ProtocolError
 
 def decodeMessageVersion(wholeMsg):
-    seq, wholeMsg = decoder.decode(wholeMsg, recursiveFlag=0)
-    ver, wholeMsg = decoder.decode(wholeMsg, recursiveFlag=0)
-    return ver
+    try:
+        seq, wholeMsg = decoder.decode(wholeMsg, recursiveFlag=0)
+        ver, wholeMsg = decoder.decode(wholeMsg, recursiveFlag=0)
+        return ver
+    except PyAsn1Error:
+        raise ProtocolError('Invalid BER at SNMP version component')
