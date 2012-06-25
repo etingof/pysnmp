1d9f36d99d4bff37affe20fd9c04d1a0abdd5c78
elie <elie>
2012-06-25 23:29:02 +0000

    handle unix pipe removal within this transport object

--- pysnmp/carrier/asyncore/dgram/unix.py
+++ pysnmp/carrier/asyncore/dgram/unix.py
@@ -1,4 +1,4 @@
-"""Implements asyncore-based UNIX transport domain"""
+# Implements asyncore-based UNIX transport domain
 from os import remove
 from socket import AF_UNIX
 from pysnmp.carrier.asynsock.dgram.base import DgramSocketTransport
@@ -8,10 +8,15 @@ domainName = snmpLocalDomain = (1, 3, 6, 1, 2, 1, 100, 1, 13)
 class UnixSocketTransport(DgramSocketTransport):
     sockFamily = AF_UNIX
 
+    def openServerMode(self, iface):
+        DgramSocketTransport.openServerMode(self, iface)
+        self.__iface = iface
+        return self
+
     def closeTransport(self):
         DgramSocketTransport.closeTransport(self)
         try:
-            remove(self._iface)
+            remove(self.__iface)
         except:
             pass
 
