476a2f92648cb2843c9badbf9b60bbcda3781b3c
elie <elie>
2012-06-30 10:08:03 +0000

    UNIX domain sockets must be explicitly bound regardless of client/server role

--- pysnmp/carrier/asyncore/dgram/unix.py
+++ pysnmp/carrier/asyncore/dgram/unix.py
@@ -1,5 +1,5 @@
 # Implements asyncore-based UNIX transport domain
-from os import remove
+from os import remove, tmpnam
 from socket import AF_UNIX
 from pysnmp.carrier.asynsock.dgram.base import DgramSocketTransport
 
@@ -7,6 +7,13 @@ domainName = snmpLocalDomain = (1, 3, 6, 1, 2, 1, 100, 1, 13)
 
 class UnixSocketTransport(DgramSocketTransport):
     sockFamily = AF_UNIX
+
+    def openClientMode(self, iface=None):
+        if iface is None:
+            iface = tmpnam()  # UNIX domain sockets must be explicitly bound
+        DgramSocketTransport.openClientMode(self, iface)
+        self.__iface = iface
+        return self
 
     def openServerMode(self, iface):
         DgramSocketTransport.openServerMode(self, iface)
