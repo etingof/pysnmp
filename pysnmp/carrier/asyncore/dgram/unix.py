2c1c5a1944e1cb8593528781c9d7dcaaf2f69221
elie <elie>
2012-07-03 11:15:53 +0000

    always craft local endpoint by ourselves

--- pysnmp/carrier/asyncore/dgram/unix.py
+++ pysnmp/carrier/asyncore/dgram/unix.py
@@ -1,16 +1,6 @@
 # Implements asyncore-based UNIX transport domain
-import os, sys
-if sys.version_info[0] < 3:
-    tmpnam = os.tmpnam
-else:
-    import random
-    random.seed()
-    def tmpnam():
-        f = ''
-        while len(f) < 8:
-            f += chr(random.randrange(65, 91)) + \
-                 chr(random.randrange(97, 123))
-        return os.path.sep + 'tmp' + os.path.sep + 'snmpsim' + f
+import os
+import random
 try:
     from socket import AF_UNIX
 except ImportError:
@@ -19,12 +9,18 @@ from pysnmp.carrier.asynsock.dgram.base import DgramSocketTransport
 
 domainName = snmpLocalDomain = (1, 3, 6, 1, 2, 1, 100, 1, 13)
 
+random.seed()
+
 class UnixSocketTransport(DgramSocketTransport):
     sockFamily = AF_UNIX
-
     def openClientMode(self, iface=None):
         if iface is None:
-            iface = tmpnam()  # UNIX domain sockets must be explicitly bound
+            # UNIX domain sockets must be explicitly bound
+            iface = ''
+            while len(iface) < 8:
+                iface += chr(random.randrange(65, 91))
+                iface += chr(random.randrange(97, 123))
+            iface = os.path.sep + 'tmp' + os.path.sep + 'pysnmp' + iface
         if os.path.exists(iface):
             os.remove(iface)
         DgramSocketTransport.openClientMode(self, iface)
