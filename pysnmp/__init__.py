4e94ec922e75e37ae29d1e74b237ab1e8a823a5e
elie <elie>
2012-07-04 16:00:33 +0000

    version is a tuple of ints

--- pysnmp/__init__.py
+++ pysnmp/__init__.py
@@ -1,5 +1,5 @@
 # http://www.python.org/dev/peps/pep-0396/
 __version__ = '4.2.3'
 # backward compatibility
-version = tuple(__version__.split('.'))
+version = tuple([ int(x) for x in __version__.split('.') ])
 majorVersionId = version[0]
