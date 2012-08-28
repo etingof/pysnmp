fd52b5bdd30cfa196f8c05b38db6dafbb33d88c1
elie <elie>
2012-08-28 15:33:03 +0000

    drop 'rc' suffix in version

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ doclines = [ x.strip() for x in __doc__.split('\n') if x ]
  
 params.update( {
     'name': 'pysnmp',
-    'version': open('pysnmp/__init__.py').read().split('\'')[1]+'rc7',
+    'version': open('pysnmp/__init__.py').read().split('\'')[1],
     'description': doclines[0],
     'long_description': ' '.join(doclines[1:]),
     'maintainer': 'Ilya Etingof <ilya@glas.net>',
