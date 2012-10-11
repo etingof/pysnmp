d4bd0fb5c30971bd7d1388f546c563c1d2208d9c
elie <elie>
2012-10-11 10:04:34 +0000

    oops, wrong place for rc

--- pysnmp/__init__.py
+++ pysnmp/__init__.py
@@ -1,5 +1,5 @@
 # http://www.python.org/dev/peps/pep-0396/
-__version__ = '4.2.4rc1'
+__version__ = '4.2.4'
 # backward compatibility
 version = tuple([ int(x) for x in __version__.split('.') ])
 majorVersionId = version[0]
