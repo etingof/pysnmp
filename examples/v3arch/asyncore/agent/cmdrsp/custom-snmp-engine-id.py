a4395a0aa9fb5a627c0e52ced82455067d48d300
elie <elie>
2012-12-14 14:35:10 +0000

    example configuration with specified USM ContextEngineId

--- examples/v3arch/asyncore/agent/cmdrsp/custom-snmp-engine-id.py
+++ examples/v3arch/asyncore/agent/cmdrsp/custom-snmp-engine-id.py
@@ -5,14 +5,14 @@
 # the following options:
 #
 # * SNMPv3
-# * with SNMP EngineID: 8000000001020304
+# * with SNMP EngineID: 8000000004030201
 # * with USM user 'usr-md5-des', auth: MD5, priv DES
 # * allow access to SNMPv2-MIB objects (1.3.6.1.2.1)
 # * over IPv4/UDP, listening at 127.0.0.1:161
 # 
 # Either of the following Net-SNMP's commands will walk this Agent:
 #
-# $ snmpwalk -v3 -u usr-md5-des -l authPriv -A authkey1 -X privkey1 -e 8000000001020304 localhost .1.3.6
+# $ snmpwalk -v3 -u usr-md5-des -l authPriv -A authkey1 -X privkey1 -e 8000000004030201 localhost .1.3.6
 #
 from pysnmp.entity import engine, config
 from pysnmp.entity.rfc3413 import cmdrsp, context
@@ -20,7 +20,7 @@ from pysnmp.carrier.asynsock.dgram import udp
 from pysnmp.proto import rfc1902
 
 # Create SNMP engine
-snmpEngine = engine.SnmpEngine(rfc1902.OctetString(hexValue='8000000001020304'))
+snmpEngine = engine.SnmpEngine(rfc1902.OctetString(hexValue='8000000004030201'))
 
 # Transport setup
 
