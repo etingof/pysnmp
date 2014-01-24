61043b0d552d8833077b39e8e817c3b36e1ac2eb
elie <elie>
2014-01-24 12:33:42 +0000

    MIB instrumentation example improved to cover table index building facility

--- examples/smi/agent/operations-on-managed-objects.py
+++ examples/smi/agent/operations-on-managed-objects.py
@@ -11,20 +11,31 @@ print('Building MIB tree...'),
 mibInstrum = instrum.MibInstrumController(mibBuilder)
 print('done')
 
-print('Remote manager write/create access to MIB instrumentation: '),
-print(mibInstrum.writeVars(
-    ( ((1,3,6,1,6,3,18,1,1,1,2,109,121,110,109,115), 'mycomm'),
-      ((1,3,6,1,6,3,18,1,1,1,3,109,121,110,109,115), 'mynmsname'),
-      ((1,3,6,1,6,3,18,1,1,1,7,109,121,110,109,115), 'volatile')) )
+print('Building table entry index from human-friendly representation...'),
+snmpCommunityEntry, = mibBuilder.importSymbols(
+    'SNMP-COMMUNITY-MIB', 'snmpCommunityEntry'
 )
+instanceId = snmpCommunityEntry.getInstIdFromIndices('my-router')
+print('done')
 
-print('Remote manager read access to MIB instrumentation (table walk)')
+print('Create/update SNMP-COMMUNITY-MIB::snmpCommunityEntry table row: ')
+varBinds = mibInstrum.writeVars(
+    ( (snmpCommunityEntry.name+(2,)+instanceId, 'mycomm'),
+      (snmpCommunityEntry.name+(3,)+instanceId, 'mynmsname'),
+      (snmpCommunityEntry.name+(7,)+instanceId, 'volatile') )
+)
+for oid, val in varBinds:
+    print('%s = %s' % ('.'.join([str(x) for x in oid]), val.prettyPrint()))
+print('done')
+
+print('Read whole MIB (table walk)')
 oid, val = (), None
 while 1:
     oid, val = mibInstrum.readNextVars(((oid, val),))[0]
     if exval.endOfMib.isSameTypeWith(val):
         break
-    print(oid, val.prettyPrint())
+    print('%s = %s' % ('.'.join([str(x) for x in oid]), val.prettyPrint()))
+print('done')
 
 print('Unloading MIB modules...'),
 mibBuilder.unloadModules()
