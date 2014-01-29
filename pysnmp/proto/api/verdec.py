26a9dd72ec39f2482ef4d873131a77c6978b0ed2
elie <elie>
2014-01-29 20:13:31 +0000

    fix to decodeMessageVersion() to withstand broken messages

--- pysnmp/proto/api/verdec.py
+++ pysnmp/proto/api/verdec.py
@@ -1,11 +1,16 @@
+from pyasn1.type import univ
 from pyasn1.codec.ber import decoder
 from pyasn1.error import PyAsn1Error
 from pysnmp.proto.error import ProtocolError
 
 def decodeMessageVersion(wholeMsg):
     try:
-        seq, wholeMsg = decoder.decode(wholeMsg, recursiveFlag=0)
-        ver, wholeMsg = decoder.decode(wholeMsg, recursiveFlag=0)
+        seq, wholeMsg = decoder.decode(
+            wholeMsg, asn1Spec=univ.Sequence(), recursiveFlag=0
+        )
+        ver, wholeMsg = decoder.decode(
+            wholeMsg, asn1Spec=Integer(), recursiveFlag=0
+        )
         return ver
     except PyAsn1Error:
         raise ProtocolError('Invalid BER at SNMP version component')
