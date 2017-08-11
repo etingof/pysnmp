78109e74eb31b456e6a1a2c5b189b786b6c94903
Ilya Etingof <etingof@gmail.com>
2017-08-11 18:32:32 +0200

    bumped up to 4.3.10

--- pysnmp/__init__.py
+++ pysnmp/__init__.py
@@ -1,5 +1,5 @@
 # http://www.python.org/dev/peps/pep-0396/
-__version__ = '4.3.9'
+__version__ = '4.3.10'
 # backward compatibility
 version = tuple([int(x) for x in __version__.split('.')])
 majorVersionId = version[0]
