98e2a3e838bf851667020b54ae8d5bdab157f5ce
Ilya Etingof <etingof@gmail.com>
2018-08-06 23:01:36 +0200

    prepare 4.4.6

--- pysnmp/__init__.py
+++ pysnmp/__init__.py
@@ -1,5 +1,5 @@
 # http://www.python.org/dev/peps/pep-0396/
-__version__ = '4.4.5'
+__version__ = '4.4.6'
 # backward compatibility
 version = tuple([int(x) for x in __version__.split('.')])
 majorVersionId = version[0]
