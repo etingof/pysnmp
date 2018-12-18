39f8b0b2839a27f45c6e153ec80d336f4ab2055b
Edgar Sousa <edgarmsousa@gmail.com>
2018-12-18 18:30:34 +0000

    Add TCP asyncore support.

--- pysnmp/hlapi/v3arch/asyncore/transport.py
+++ pysnmp/hlapi/v3arch/asyncore/transport.py
@@ -9,9 +9,63 @@ import sys
 
 from pysnmp import error
 from pysnmp.carrier.asyncore.dgram import udp, udp6
+from pysnmp.carrier.asyncore.stream import tcp
 from pysnmp.hlapi.transport import AbstractTransportTarget
 
-__all__ = ['Udp6TransportTarget', 'UdpTransportTarget']
+__all__ = ['Udp6TransportTarget', 'UdpTransportTarget', 'TcpTransportTarget']
+
+class TcpTransportTarget(AbstractTransportTarget):
+    """Creates TCP/IPv4 configuration and initialize socket API.
+    This object can be used for adding new entries to Local Configuration
+    Datastore (LCD) managed by :py:class:`~pysnmp.hlapi.SnmpEngine`
+    class instance.
+
+    See :RFC:`1906#section-3` for more information on the UDP transport mapping.
+
+    Parameters
+    ----------
+    transportAddr: :py:class:`tuple`
+        Indicates remote address in Python :py:mod:`socket` module format
+        which is a tuple of FQDN, port where FQDN is a string representing
+        either hostname or IPv4 address in quad-dotted form, port is an
+        integer.
+    timeout: :py:class:`int`
+        Response timeout in seconds.
+    retries: :py:class:`int`
+        Maximum number of request retries, 0 retries means just a single
+        request.
+    tagList: :py:class:`str`
+        Arbitrary string that contains a list of space-separated tag
+        strings used to select target addresses and/or SNMP configuration
+        (see :RFC:`3413#section-4.1.1`, :RFC:`2576#section-5.3` and
+        :py:class:`~pysnmp.hlapi.CommunityData` object).
+
+    Examples
+    --------
+    >>> from pysnmp.hlapi.v3arch.asyncore import UdpTransportTarget
+    >>> TcpTransportTarget(('demo.snmplabs.com', 161))
+    TcpTransportTarget(('195.218.195.228', 161), timeout=1, retries=5, tagList='')
+    >>>
+
+    """
+    transportDomain = tcp.domainName
+    protoTransport = tcp.TcpSocketTransport
+
+    def openClientMode(self):
+        self.transport = self.protoTransport().openClientMode(self.iface, self.transportAddr)
+        return self.transport
+
+    def _resolveAddr(self, transportAddr):
+        try:
+            return socket.getaddrinfo(transportAddr[0],
+                                      transportAddr[1],
+                                      socket.AF_INET,
+                                      socket.SOCK_STREAM,
+                                      socket.IPPROTO_TCP)[0][4][:2]
+        except socket.gaierror:
+            raise error.PySnmpError('Bad IPv4/TCP transport address %s: %s' % (
+                '@'.join([str(x) for x in transportAddr]), sys.exc_info()[1]))
+
 
 
 class UdpTransportTarget(AbstractTransportTarget):
