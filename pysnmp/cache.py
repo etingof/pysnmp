49af9176c2bb92061e38d2988fafcbf6bcd27ca6
Ilya Etingof <etingof@gmail.com>
2018-12-29 23:45:49 +0100

    Extend copyright notice to year 2019

--- pysnmp/cache.py
+++ pysnmp/cache.py
@@ -1,7 +1,7 @@
 #
 # This file is part of pysnmp software.
 #
-# Copyright (c) 2005-2018, Ilya Etingof <etingof@gmail.com>
+# Copyright (c) 2005-2019, Ilya Etingof <etingof@gmail.com>
 # License: http://snmplabs.com/pysnmp/license.html
 #
 # Limited-size dictionary-like class to use for caches
