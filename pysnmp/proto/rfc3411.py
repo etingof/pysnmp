c5f7842c430ad3431bee793362ef11cff9f1e0de
Ilya Etingof <etingof@gmail.com>
2019-02-12 00:01:03 +0100

    Fix Py2.6 incompatibility
    Specifically, set literals not yet supported.

--- pysnmp/proto/rfc3411.py
+++ pysnmp/proto/rfc3411.py
@@ -6,50 +6,50 @@
 #
 from pysnmp.proto import rfc1157, rfc1905
 
-READ_CLASS_PDUS = {
-    rfc1157.GetRequestPDU.tagSet,
-    rfc1157.GetNextRequestPDU.tagSet,
-    rfc1905.GetRequestPDU.tagSet,
-    rfc1905.GetNextRequestPDU.tagSet,
-    rfc1905.GetBulkRequestPDU.tagSet
-}
+READ_CLASS_PDUS = set(
+    (rfc1157.GetRequestPDU.tagSet,
+     rfc1157.GetNextRequestPDU.tagSet,
+     rfc1905.GetRequestPDU.tagSet,
+     rfc1905.GetNextRequestPDU.tagSet,
+     rfc1905.GetBulkRequestPDU.tagSet)
+)
 
-WRITE_CLASS_PDUS = {
-    rfc1157.SetRequestPDU.tagSet,
-    rfc1905.SetRequestPDU.tagSet
-}
+WRITE_CLASS_PDUS = set(
+    (rfc1157.SetRequestPDU.tagSet,
+     rfc1905.SetRequestPDU.tagSet)
+)
 
-RESPONSE_CLASS_PDUS = {
-    rfc1157.GetResponsePDU.tagSet,
-    rfc1905.ResponsePDU.tagSet,
-    rfc1905.ReportPDU.tagSet
-}
+RESPONSE_CLASS_PDUS = set(
+    (rfc1157.GetResponsePDU.tagSet,
+     rfc1905.ResponsePDU.tagSet,
+     rfc1905.ReportPDU.tagSet)
+)
 
-NOTIFICATION_CLASS_PDUS = {
-    rfc1157.TrapPDU.tagSet,
-    rfc1905.SNMPv2TrapPDU.tagSet,
-    rfc1905.InformRequestPDU.tagSet
-}
+NOTIFICATION_CLASS_PDUS = set(
+    (rfc1157.TrapPDU.tagSet,
+     rfc1905.SNMPv2TrapPDU.tagSet,
+     rfc1905.InformRequestPDU.tagSet)
+)
 
-INTERNAL_CLASS_PDUS = {
-    rfc1905.ReportPDU.tagSet
-}
+INTERNAL_CLASS_PDUS = set(
+    (rfc1905.ReportPDU.tagSet,)
+)
 
-CONFIRMED_CLASS_PDUS = {
-    rfc1157.GetRequestPDU.tagSet,
-    rfc1157.GetNextRequestPDU.tagSet,
-    rfc1157.SetRequestPDU.tagSet,
-    rfc1905.GetRequestPDU.tagSet,
-    rfc1905.GetNextRequestPDU.tagSet,
-    rfc1905.GetBulkRequestPDU.tagSet,
-    rfc1905.SetRequestPDU.tagSet,
-    rfc1905.InformRequestPDU.tagSet
-}
+CONFIRMED_CLASS_PDUS = set(
+    (rfc1157.GetRequestPDU.tagSet,
+     rfc1157.GetNextRequestPDU.tagSet,
+     rfc1157.SetRequestPDU.tagSet,
+     rfc1905.GetRequestPDU.tagSet,
+     rfc1905.GetNextRequestPDU.tagSet,
+     rfc1905.GetBulkRequestPDU.tagSet,
+     rfc1905.SetRequestPDU.tagSet,
+     rfc1905.InformRequestPDU.tagSet)
+)
 
-UNCONFIRMED_CLASS_PDUS = {
-    rfc1157.GetResponsePDU.tagSet,
-    rfc1905.ResponsePDU.tagSet,
-    rfc1157.TrapPDU.tagSet,
-    rfc1905.ReportPDU.tagSet,
-    rfc1905.SNMPv2TrapPDU.tagSet
-}
+UNCONFIRMED_CLASS_PDUS = set(
+    (rfc1157.GetResponsePDU.tagSet,
+     rfc1905.ResponsePDU.tagSet,
+     rfc1157.TrapPDU.tagSet,
+     rfc1905.ReportPDU.tagSet,
+     rfc1905.SNMPv2TrapPDU.tagSet)
+)
