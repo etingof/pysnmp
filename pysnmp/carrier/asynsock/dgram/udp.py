b186168d5b47c74c1d20e80939d3e22e561fe2cf
Ilya Etingof <etingof@gmail.com>
2019-02-26 09:47:12 +0100

    Drop legacy `pysnmp.carrier.asynsock`
    The sub-package is now known under the name `asyncore`
    because of the presence of many other event loops.

--- pysnmp/carrier/asynsock/dgram/udp.py
+++
@@ -1,7 +0,0 @@
-#
-# This file is part of pysnmp software.
-#
-# Copyright (c) 2005-2019, Ilya Etingof <etingof@gmail.com>
-# License: http://snmplabs.com/pysnmp/license.html
-#
-from pysnmp.carrier.asyncore.dgram.udp import *
